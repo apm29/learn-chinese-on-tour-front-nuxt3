738572b83d5c7e7fe24c2bde4a6d0848d350c2a7
apm29 <yjw999wow@163.com>
2024-03-08 17:05:58 +0800

    feat: pm2 config

---
+++ ecosystem.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: 'lcot-next',
+      exec_mode: 'cluster',
+      instances: 'max',
+      script: '.output/server/index.mjs',
+      args: 'start',
+      env: {
+        "PORT": 4000,
+      },
+    }
+  ]
+}
