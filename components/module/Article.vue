a079298a67ad8f7e42a5aa73568749af9c742c52
apm29 <yjw999wow@163.com>
2024-03-15 15:35:20 +0800

    feat: article load indicator

--- components/module/Article.vue
+++ components/module/Article.vue
@@ -5,19 +5,32 @@ const props = defineProps({
     required: true,
   },
 })
-const { data: modules } = await useFetch(`/api/module-article/${props.module}`)
+const { data: modules, pending } = await useFetch(
+  `/api/module-article/${props.module}`,
+  {
+    lazy: true
+  }
+)
 </script>
 
 <template>
   <div class="py-6">
-    <template v-for="article of modules" :key="article.id">
-      <ModuleArticleGlance
-        :article="article"
-      />
-    </template>
-    <div v-if="!modules || !modules.length" class="text-gray-400">
-      {{ $t('No data') }}
+    <div class="flex items-center space-x-4" v-if="pending">
+      <USkeleton class="h-12 w-12" :ui="{ rounded: 'rounded-full' }" />
+      <div class="space-y-2">
+        <USkeleton class="h-4 w-[250px]" />
+        <USkeleton class="h-4 w-[200px]" />
+      </div>
     </div>
+    <template v-else>
+      <template v-for="article of modules" :key="article.id">
+        <ModuleArticleGlance :article="article" />
+      </template>
+      <div v-if="!modules || !modules.length" class="text-gray-400">
+        <UIcon name="i-mdi-database-search-outline" class="inline-block align-middle text-[1em]"></UIcon>
+        {{ $t('No data') }}
+      </div>
+    </template>
   </div>
 </template>
 
