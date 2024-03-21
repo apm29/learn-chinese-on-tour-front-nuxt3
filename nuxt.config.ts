6ae218ff61aebd274de75feecde05dd103bf6887
apm29 <yjw999wow@163.com>
2024-03-21 16:35:43 +0800

    feat: tour

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -115,7 +115,7 @@ export default defineNuxtConfig({
     duration: 700, // values from 0 to 3000, with step 50ms
     easing: 'ease', // default easing for AOS animations
     once: false, // whether animation should happen only once - while scrolling down
-    mirror: true, // whether elements should animate out while scrolling past them
+    mirror: false, // whether elements should animate out while scrolling past them
     anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
   },
 
--- pages/live/tour.vue
+++ pages/live/tour.vue
@@ -1,11 +1,218 @@
 <template>
-  <div class="px-10 py-12 flex flex-col gap-4">
-    <div class="item">12</div>
-    <div class="item" data-aos="fade-up">12</div>
-    <div class="item">12</div>
-    <div class="item">12</div>
-    <div class="item">12</div>
-  </div>
+  <article class="prose prose-stone lg:max-w-[1000px] dark:prose-invert mx-auto px-4 pt-10">
+    <div class="ppt-section page-1">
+      <h1 data-aos="fade-up">The Paradise City Hangzhou</h1>
+      <p class="indent-[2em]">
+        <UImage class="max-w-full sm:max-w-[600px] mx-0 sm:mr-8 mt-0" data-aos="fade-right" align='left'
+          src="/tour/image2.jpeg"></UImage>
+        Hangzhou is the capital of Zhejiang province and the provincial economic, cultural, science and educational
+        centre. It is one of the central cities in the Yangtze River Delta. As a famous scenic tourist city, it ranks
+        among the first batch of national historical and cultural cities. Located on the south side of the Delta
+        region
+        and at the western end of Hangzhou Bay, it is the extended intersection of the <strong>Silk Road Economic
+          Belt</strong> and the 21st Century Maritime Silk Road and a strategic hub along the Online Silk Road.
+      </p>
+    </div>
+    <div class="ppt-section page-2">
+      <h1 data-aos="fade-up">Hangzhou History</h1>
+      <p class="indent-[2em]">
+        <UImage class="max-w-full sm:max-w-[40%]  mx-0 sm:mx-4" data-aos="fade-left" align='right'
+          src="/tour/image4.jpeg"></UImage>
+
+        One of the birthplaces of Chinese civilisation and a famous prefecture of the Southeast in history, Hangzhou
+        ranks among the seven ancient capitals of China. First established as a county in the Qin dynasty(222BC), it
+        has a history of over 2,200 years.Hangzhou was described by the ltalian traveler Marco Polo in the 13th
+        century as “the finest and most splendid city in the world” .
+      </p>
+      <p>
+        In recent years, Hangzhou has won a series of awards ：
+      <ul>
+        <li>the China's Top 10 Most Influential Cities,</li>
+        <li>China's Top 10 Most Innovative Cities,</li>
+        <li>China's Top 10 Smartest Cities,</li>
+        <li>China's Happiest City for16 consecutive years，</li>
+        <li>the World's Top 15 Model Cities for Best Practices in Tourism.</li>
+      </ul>
+      </p>
+      <UImage class="max-w-full sm:max-w-[50%]  mx-0 sm:mx-4" data-aos="fade-up" align="left" src="/tour/image5.png">
+      </UImage>
+    </div>
+    <div class="ppt-section page-3">
+      <h1 data-aos="fade-up">Hangzhou Food</h1>
+      <p class="indent-[2em]">
+        <UImage class="max-w-full sm:max-w-[30%] mx-0 sm:mx-4" data-aos="fade-left" align="right"
+          src="/tour/image6.png">
+        </UImage>
+        Zhejiang cuisine is one of the eight major cuisines in China.Hangzhou dishes, an important part of Zhejiang
+        food culture, represents an important style of Zhejiang cuisine.
+        Famous Dishes
+        Shrimp Meat with Longjing Tea Leaves,West Lake Vinegar Fish, Beggar's Chicken,Dongpo Pork, Braised Spring
+        Bamboo Shoots,Hangzhou Duck in Brown Sauce,Stewed Duck with Dried Bamboo Shoots, West Lake Water Shield
+        Soup,Sautéed Eel Slices, etc.
+
+      </p>
+
+      <div class="pic-gallery relative" data-aos="fade-right">
+
+        <UImage
+          class="shadow-md filter drop-shadow-lg max-w-full sm:max-w-[20%] sm:absolute left-1/4 top-12 mx-0 sm:mx-1"
+          src="/tour/image8.jpeg"> </UImage>
+        <UImage class="shadow-md filter drop-shadow-lg max-w-full sm:max-w-[30%] sm:absolute left-0 top-0 mx-0 sm:mx-1"
+          src="/tour/image7.jpeg"></UImage>
+        <UImage
+          class="shadow-md filter drop-shadow-lg max-w-full sm:max-w-[20%] sm:absolute left-1/2 top-0 mx-0 sm:mx-1"
+          src="/tour/image9.jpeg"></UImage>
+        <UImage
+          class="shadow-md filter drop-shadow-lg max-w-full sm:max-w-[20%] sm:absolute left-1/3 top-2 mx-0 sm:mx-1"
+          src="/tour/image10.jpeg"></UImage>
+      </div>
+    </div>
+    <div class="ppt-section page-4">
+      <h1>
+        Three World Heritage Sites
+      </h1>
+      <div class="clear-both">
+        <h2>West Lake Scenic Area</h2>
+        <p class="indent-[2em]">
+          <UImage class="max-w-full sm:max-w-[50%] mt-0 mx-0 sm:mr-4" data-aos="fade-right" align="left"
+            src="/tour/image11.png">
+          </UImage>
+          West Lake is the root and soul of Hangzhou. It is world-famous for its picturesque landscape, long history,
+          profound culture and heritage sites. It is a culturally renowned lake with the most outstanding spiritual
+          dwelling function in Chinese history, and the “Paradise on Earth” known both at home and abroad. The WLSA is
+          surrounded by green trees, lush hills, bubbling streams and murmuring creeks,with different scenes in spring,
+          summer,autumn and winter.
+          In 2011,the West Lake Cultural Landscape of Hangzhou was included in the World Heritage List.
+        </p>
+        <br />
+      </div>
+
+      <div class="clear-both">
+        <h2>The Grand Canal Hangzhou</h2>
+        <p class="indent-[2em]">
+          <UImage class="max-w-full sm:max-w-[50%] mt-0 mx-0 sm:ml-4" data-aos="fade-left" align="right"
+            src="/tour/image12.jpeg">
+          </UImage>
+          The Beijing-Hangzhou Grand Canal is an essential part of China's canal system. It is the longest and largest
+          ancient canal and one of the oldest canals in the world. In the Hangzhou section of the Grand Canal can be
+          seen
+          various Qing dynasty houses with white walls and black tiles, connected by bluish-white-slab-paved lanes and
+          alleys.In 2014,China's Grand Canal project was inscribed on the World Heritage List.
+
+        </p>
+      </div>
+      <div class="clear-both">
+        <h2>Archaeological Ruins of Liangzhu City</h2>
+        <p class="indent-[2em]">
+          <UImage class="max-w-full sm:max-w-[40%] mt-0 mx-0 sm:mr-4" data-aos="fade-right" align="left"
+            src="/tour/image13.jpeg">
+          </UImage>
+          As it is the first Neolithic city site found in the lower reaches of the Yangtze River, it has become known as
+          “China's First City”.The peripheral water conservancy system marks the earliest large water engineering site
+          ever discovered in China. Bearing testimony to the 5,000-year history of Chinese civilisation, the
+          Archaeological Ruins of Liangzhu City was inscribed onto the World Heritage List in 2019.
+
+
+        </p>
+      </div>
+    </div>
+
+    <div class="ppt-section page-5">
+      <h1 data-aos="fade-up">Specialities from Greater Hangzhou</h1>
+
+      <h2>
+        West Lake Landscape Tour
+      </h2>
+      <p>
+        Leifeng Pagoda in Evening Glow <strong> →</strong> Viewing Fish at Flower Pool
+        <strong> →</strong>West Lake Boat Cruise<strong> →</strong>Lingyin Temple （including Peak Flying from Afar)
+      </p>
+      <div class="pic-gallery relative" data-aos="fade-up">
+
+        <UImage class="shadow-md filter drop-shadow-lg max-w-full sm:max-w-[30%] sm:absolute left-0 top-12 mx-0 sm:mx-1"
+          src="/tour/image16.jpeg"> </UImage>
+        <UImage
+          class="shadow-md filter drop-shadow-lg max-w-full sm:max-w-[30%] sm:absolute left-1/3 top-0 mx-0 sm:mx-1"
+          src="/tour/image15.jpeg"></UImage>
+        <UImage
+          class="shadow-md filter drop-shadow-lg max-w-full sm:max-w-[30%] sm:absolute left-2/3 top-12 mx-0 sm:mx-1"
+          src="/tour/image14.jpeg"></UImage>
+      </div>
+    </div>
+    <div class="ppt-section page-5">
+
+      <h2>
+        Tea Capital Experience Tour
+      </h2>
+      <p>
+        China National Tea Museum <strong> →</strong>Rediscovering Tea at Dragon Well<strong> →</strong>Meijiawu Tea
+        Village<strong> →</strong>Jingshan Tea Ceremony
+      </p>
+      <div class="pic-gallery text-center sm:grid sm:grid-cols-2  sm:grid-row-3 lg:grid-cols-3 lg:grid-row-2 gap-2"
+        data-aos="fade-up">
+
+        <UImage class="gallery-item" src="/tour/image18.jpeg"> </UImage>
+        <UImage class="gallery-item  " src="/tour/image17.jpeg"></UImage>
+        <UImage class="gallery-item" src="/tour/image19.jpeg"></UImage>
+
+        <UImage class="gallery-item" src="/tour/image21.jpeg"></UImage>
+        <UImage class="gallery-item" src="/tour/image20.jpeg"> </UImage>
+
+        <UImage class="gallery-item" src="/tour/image22.jpeg"></UImage>
+      </div>
+    </div>
+    <div class="ppt-section page-6">
+
+      <h2>
+        Southern Song Charm Seeking Tour
+      </h2>
+      <p>
+        Southern Song Imperial Street<strong> →</strong>Dajing Alley<strong> →</strong>Hu Qing Yu Tang (Museum of
+        Traditional Chinese Medicine) <strong> →</strong> Jiangnan Copper House <strong> →</strong> City God Pavilion
+        <strong> →</strong>Southern Song Dynasty Imperial Kiln Museum
+      </p>
+      <div class="pic-gallery text-center sm:grid sm:grid-cols-2  sm:grid-row-3 lg:grid-cols-3 lg:grid-row-2 gap-2"
+        data-aos="fade-up">
+
+        <UImage class="gallery-item" src="/tour/image27.jpeg"> </UImage>
+        <UImage class="gallery-item  " src="/tour/image23.jpeg"></UImage>
+        <UImage class="gallery-item" src="/tour/image24.jpeg"></UImage>
+
+        <UImage class="gallery-item" src="/tour/image25.jpeg"></UImage>
+        <UImage class="gallery-item" src="/tour/image26.jpeg"> </UImage>
+
+      </div>
+    </div>
+    <div class="ppt-section page-7">
+
+      <h2>
+        West Lake & Xixi Wetland Eco-Tour
+      </h2>
+      <p>
+        Orioles Singing in the Willows<strong> →</strong>Breeze-rustled Lotus at Quyuan Garden<strong> →</strong>
+        Maojiabu Village<strong> →</strong> Xixi National Wetland Park
+
+      </p>
+      <UImage class="gallery-item" src="/tour/image28.jpeg"> </UImage>
+    </div>
+
+    <div class="ppt-section page-8">
+
+      <h2>
+        Water Town Discovery Tour
+      </h2>
+      <p>
+        City <strong> →</strong> Wuzhen water town
+      </p>
+      <div class="pic-gallery text-center sm:grid sm:grid-cols-2 sm:grid-row-3 gap-2"
+        data-aos="fade-up">
+
+        <UImage class="gallery-item" src="/tour/image31.jpeg"> </UImage>
+        <UImage class="gallery-item  " src="/tour/image30.jpeg"></UImage>
+
+      </div>
+    </div>
+  </article>
 </template>
 
 <script setup>
@@ -13,10 +220,11 @@
 </script>
 
 <style scoped>
-.scroll-section {
-  @apply min-h-screen w-full relative;
+.ppt-section {
+  @apply min-h-screen w-full relative clear-both;
 }
-.item {
-  @apply min-h-32 min-w-32 bg-gray-200;
+
+.gallery-item {
+  @apply shadow-lg filter drop-shadow-lg w-full sm:h-[320px] object-cover rounded-lg;
 }
 </style>
